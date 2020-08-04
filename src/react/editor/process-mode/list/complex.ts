8c940db01c87491209d831192a7a764255f8abdc
MyAeroCode <meteo_meteo@naver.com>
2020-08-04 16:46:13 +0900

    update: COMPLEX 모드 구현

--- src/react/editor/process-mode/list/complex.ts
+++ src/react/editor/process-mode/list/complex.ts
@@ -1,12 +1,19 @@
 import { ProcessMode } from "../process-mode";
+import { IMAGE_REPLACER_MAPPER } from "./image-replacer-mapper";
+import { BR_RECOVER } from "./br-recover";
 
 /**
- * BR 태그를 복구를 복구 한 뒤,
- * 이미지 치환자를 변환하는 모드.
+ * 이미지 치환자를 변환 한 뒤,
+ * BR 태그를 복구하는 모드.
  */
 export const COMPLEX = new ProcessMode({
     name: "Complex",
     process: (context) => {
-        return `[${context.docsTitle}] COMPLEX \n${context.inputText}\n`;
+        let ctx = Object.assign({}, context);
+        const chain: ProcessMode[] = [IMAGE_REPLACER_MAPPER, BR_RECOVER];
+        for (const mode of chain) {
+            ctx.inputText = mode.process(ctx);
+        }
+        return ctx.inputText;
     },
 });
