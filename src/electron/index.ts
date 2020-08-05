1d6395b9a26447ed4537fbd5cfd389419d4671dd
MyAeroCode <meteo_meteo@naver.com>
2020-08-05 18:30:11 +0900

    update: 개발모드와 빌드모드 분리

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -1,4 +1,6 @@
 import { app, BrowserWindow } from "electron";
+import fs from "fs";
+import path from "path";
 
 app.whenReady().then(() => {
     const window = new BrowserWindow({
@@ -8,5 +10,14 @@ app.whenReady().then(() => {
         resizable: false,
     });
     window.maximize();
-    window.loadURL("http://localhost:8080");
+
+    //
+    // 빌드된 파일이 존재한다면 그것을 불러온다.
+    const reactPath = path.join(__dirname, "../../dist/react");
+    const indexPath = path.join(__dirname, "../../index.html");
+    if (fs.existsSync(reactPath)) {
+        window.loadFile(indexPath);
+    } else {
+        window.loadURL("http://localhost:8080");
+    }
 });
